79f9e45e2f0ca01c96144f3f3124565f4487b50c
Mariaesteves <mariaferraesteves@hotmail.com>
2025-04-07 15:54:09 +0100

    Mini-project day 1| solved

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,43 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex];
+    }
 
-    // 2. getQuestion()
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }  
     
-    // 3. moveToNextQuestion()
+    shuffleQuestions(){
+        for (let i= this.questions.length-1; i>0; i--){
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+        return this.questions;
+    }
 
-    // 4. shuffleQuestions()
-
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
+    checkAnswer (answer){
+        if (answer === this.getQuestion().answer){
+            this.correctAnswers++;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    hasEnded(){
+        if (this.currentQuestionIndex >= this.questions.length){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
+
